0b094286118e96146f93ff0839d497316a6ada6c
Hengyu <h3ngyu@gmail.com>
2023-11-18 01:40:23 +0800

    Fix: date display

--- webapp/src/features/viewSlice.ts
+++ webapp/src/features/viewSlice.ts
@@ -6,6 +6,25 @@ type State = {
     currentInstance: RobotInstance
 }
 
+const padTo2Digits = (num: number) => {
+    return num.toString().padStart(2, '0');
+}
+const formatDate = (date: Date) => {
+    return (
+        [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+        ].join('-') +
+        'T' +
+        [
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
+        ].join(':')
+    );
+}
+
 const emptyInstance: RobotInstance = {bid: "", name: "", token: "", messages: []}
 
 const initialState: State = {
@@ -23,7 +42,7 @@ export const viewSlice = createSlice({
             const {messageText, sender} = action.payload;
             state.currentInstance.messages.push({
                 messageText,
-                messageTime: String(new Date()),
+                messageTime: formatDate(new Date()),
                 sender,
             })
         }
